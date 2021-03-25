b22550a010d71bc389d0d384b492b318b57b2322
luanaflorencio <lua.m.016@gmail.com>
2021-03-25 14:50:31 -0300

    Construção das classes

---
+++ pokemon.js
@@ -0,0 +1,12 @@
+class Pokemon {
+    constructor (nome,tipo,poder) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.poder = poder;
+    }
+    getClass() {
+        return ;
+    }
+}
+
+   
---
+++ treinador.js
@@ -0,0 +1,15 @@
+class Treinador {
+    constructor(nome, idade, poke1, poke2, poke3) {
+        this.nome = nome;
+        this.idade = idade;
+        this.poke1 = poke1;
+        this.poke2 = poke2;
+        this.poke3 = poke3;
+    }
+    getClass() {
+        return ;
+    }
+
+
+}
+
